a2195e3353848a0e676bb35d22f3de398ad7466e
aaparky <aa.parky@gmail.com>
2023-11-06 14:15:48 +0000

    layout tests

--- index.php
+++ index.php
@@ -47,46 +47,158 @@
         <hr>
 
         <div>
-            <div>
+            <div class="headlines">
                 <h1>HEADLINES</h1>
             </div>
         </div>
 
-        <div>
+        <div class="news-columns-top">
 
-            <div>
-                <div>
-                    <h4>Sub Headline</h4>
-                    <hr><img src="https://bootsnipp.com/bootstrap-builder/libs/builder/icons/image.svg" height="128"
-                        width="128">
-                    <p>By reporter name and title</p>
-                    <p>Here is the news article!Here is the news article!Here is the news article!Here is the news
-                        article!Here is the news article!Here is the news article!Here is the news article!Here is the
-                        news article!Here is the news article!Here is the news article!Here is the news article!Here is
-                        the news article!Here is the news article!Here is the news article!Here is the news article!</p>
+            <div class="card mt-8" style="width: 20rem;">
+                <img src="https://bootsnipp.com/bootstrap-builder/libs/builder/icons/image.svg" height="28" width="28"
+                    class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Mrglglglgl! Rumblecoin’s deals be the best, coin-lovin' gibblewrench lorem
+                        ipsum gallywix gold. Azeroth's finest blingfitz and steamwiggle, glimmering azurite trinkets to
+                        gizmocopter ventures, aye! Estate-plunder and pocket-fillin', sparkjolt hustlebuzz in the
+                        tradewinds market—glittering and glinting with every mekgineer's dream.
+                        Gazlowe-approved, copperpinch lorem ipsum, grime-encrusted cogspark to the gold-grubbin'
+                        loot-lifter, from Kajaro to the Slippery Slopes of Kezan.
+                        Gold-hungry, loot-lovin’, trinket-totin' entrepreneurs of fortune, we never goblin up the chance
+                        to turn a profit, no sir! From the gravesite to the bank vault, trust in the Undermine
+                        Undertakers—where every deal's a steal and every steal's a deal, mrglglglgl!
+                    </p>
                 </div>
             </div>
 
-            <div>
-                <div>
-                    <h4>text only</h4>
-                    <p>many many news!many many news!many many news!many many news!many many news!many many news!many
-                        many news!many many news!many many news!many many news!many many news!many many news!many many
-                        news!many many news!many many news!many many news!many many news!many many news!many many
-                        news!many many news!many many news!many many news!many many news!many many news!many many
-                        news!many many news!many many news!many many news!many many news!many many news!many many
-                        news!many many news!many many news!many many news!many many news!many many news!many many news!
+            <div class="card mt-8">
+                <img src="/images/gob-kungfu.png" alt="..." width="708">
+                <div id="gob-photo">Picture by ...</div>
+            </div>
+
+            <div class="card mt-8" style="width: 12rem;">
+                <img src="https://bootsnipp.com/bootstrap-builder/libs/builder/icons/image.svg" height="28" width="28"
+                    class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Goblin Tools</h5>
+                    <p class="card-text">
+                        ADVERTISE HERE!
+                    </p>
+
+                </div>
+            </div>
+
+        </div>
+
+        <div class="news-columns-mid">
+            <div class="card mt-8" style="width: 60%;">
+                <div class="card-body">
+                    <p class="card-text">
+                        No hexweave bag too big, no loot too
+                        lewt! Undertakin' with a side of goblin-tech innovation, all guaranteed by the Bilgewater Cartel
+                        lorem ipsum—savvy?
+                        Kablooey kablam, estate-bargain blastpowder deals, shrewd and shinyslick, Lastrights Rumblecoin
+                        don't dabble in no dud-loot fizzles! Mrglglglgl! Tinkerin' with the thunderous thumpwhistle of a
+                        hundred goblin workshops—grease-stained plans for the afterlife, to the battle-boom of the
+                        Undermine's deepest lorem ipsum.
+
+                        Hustle and bustle, coin clinkin', and auction housin'—never a dull shard when the Undertakers
+                        are in town. From the fizzletorque flimflams to the zappyzoom of a goblin rocket, we've got your
+                        back in the hardcore realms, mrglglglgl! So, when the heroes fall, Rumblecoin's crew will be
+                        there with a deal and a dynamite sendoff, Azeroth-style!
+
+                        Gold-hungry, loot-lovin’, trinket-totin' entrepreneurs of fortune, we never goblin up the chance
+                        to turn a profit, no sir! From the gravesite to the bank vault, trust in the Undermine
+                        Undertakers—where every deal's a steal and every steal's a deal, mrglglglgl!</p>
+                </div>
+            </div>
+
+            <div class="card mt-8" style="width: 40%;">
+                <div class="card-body">
+                    <p class="card-text">
+                        Stockmarket
+                    <ul>
+                        <li><strong>BBE</strong> - as volatile as a barrel of Blast-O-Powder.</li>
+                        <li><strong>BB&PP</strong> - the shadowy side of goblin politics.</li>
+                        <li><strong>GG&SS</strong> - Want a piece of the Page 3 action?</li>
+                        <li><strong>EME</strong> - Goblinwood blockbusters</li>
+                        <li><strong>FF&00</strong> - Bet on the endgame. Morbid but reliable stocks.</li>
+                        <li><strong>S&SC</strong> - the best informants.</li>
+                        <li><strong>SS&GG</strong> - Whether it's Ogre wrestling or Mechanostrider racing.</li>
+                    </ul>
+                    </p>
+                </div>
+            </div>
+
+            <div class="card mt-8" style="width: 30%;">
+                <div class="card-body">
+                    <p class="card-text">
+                        Index:
+                    <ul>
+                        <li>Business</li>
+                        <li>Politics</li>
+                        <li>Page 3</li>
+                        <li>Entertainment</li>
+                        <li>Obituaries</li>
+                        <li>Classifieds</li>
+                        <li>Sports</li>
+                    </ul>
                     </p>
                 </div>
             </div>
 
         </div>
 
-        <footer>
-            <p>© the Undermine Undertaker's Update 2017</p>
-        </footer>
+        <div class="news-columns-bottom">
+
+            <div class="card mt-8" style="width: 20%;">
+                <div class="card-body">
+                    <p class="card-text">
+                        ADVERTISE HERE!
+                    </p>
+                </div>
+            </div>
+
+            <div class="card mt-8" style="width: 30%;">
+                <div class="card-body">
+                    <p class="card-text">
+                        Index:
+                    <ul>
+                        <li>Business</li>
+                        <li>Politics</li>
+                        <li>Page 3</li>
+                        <li>Entertainment</li>
+                        <li>Obituaries</li>
+                        <li>Classifieds</li>
+                        <li>Sports</li>
+                    </ul>
+                    </p>
+                </div>
+            </div>
+
+            <div class="card mt-8" style="width: 60%;">
+                <div class="card-body">
+                    <p class="card-text">
+                        Bargains blasted out of the boom-boom room, shiny and shrewd, Lastrights ain't no common loot-monger. Mrglglglgl! We wheel, we deal, we steal (but only the best deals, promise!), in the grand goblin tradition of profit above all! Every hero's end is a new profit trend—estate services with the Rumblecoin guarantee, glimmering with the sheer brilliance of a Blingtron circuit board.
+
+                        Tick-tock, time's a-wastin'—time is money, friend! And our time's spent ensuring the finest fizzletop memorials and money-makin' opportunities, from this world to the next. Venture Company moguls and Bilgewater barons, they all know the name Lastrights Rumblecoin—master of coinage, undertaker extraordinaire, mrglglglgl!
+                        
+                        From the shiny shores of Silvermoon to the musty money vaults of Ironforge, our goblin fingers are in every pie—rich with the aroma of profit. Deals done dirt cheap, but never cheap on quality, no sir! In Hardcore mode, when the stakes are high, trust in the Undermine Undertakers to carry your legacy... and your bags of gold, mrglglglgl!</p>
+                </div>
+            </div>
+
+        </div>
 
     </div> <!-- /container -->
+
+    <footer class="footer">
+        <div id="flex-footer">
+            <div id="flex-footer-left">the Undermine Undertaker's Update 2017</div>
+            <div id="flex-footer-centre">Centre</div>
+            <div id="flex-footer-right">Right</div>
+        </div>
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
