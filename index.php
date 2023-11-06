1b69d0e1e561a1a83c74f9c8bb51a88be838eaa8
aaparky <aa.parky@gmail.com>
2023-11-06 19:24:43 +0000

    more fonts

--- index.php
+++ index.php
@@ -8,9 +8,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- My CSS -->
-    <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/paper.css">
     <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/main.css">
     <!-- adobe fonts -->
     <link rel="stylesheet" href="https://use.typekit.net/tkq4iko.css">
 </head>
@@ -28,7 +29,7 @@
             </div>
 
             <div id="flex-header-center">
-                <h2>The Undermine Undertaker's Update</h2>
+                The Undermine Undertaker's Update
             </div>
             <div id="flex-header-right">
                 Daily
@@ -42,7 +43,7 @@
                 Date &amp; Time
             </div>
             <div id="sub-vol">
-                Volume # &amp; Issue #
+                Volume 1 &amp; Issue 1
             </div>
             <div id="sub-est">
                 EST.2023 1c
@@ -68,7 +69,7 @@
                             <span class="author">by Wizzik Sprocketslip</span>
                         </div>
                         <h5 class="card-title">'final service extravaganza'</h5>
-                        <p class="card-text">
+                        <p class="card-text typewritter">
                             In the bustling bazaar of Ratchet, where the clinks of gold are as common as the clatter of
                             gears, a peculiar scene unfolded that even the most brazen buccaneer couldn't help but gawk
                             at. The Undermine Undertakers, Azeroth's premier post-mortem property purveyors, found
@@ -76,7 +77,7 @@
                             clamoring to join the ranks of the most spirited (and profitable) enterprise this side of
                             the Great Sea.
                         </p>
-                        <p>
+                        <p class="card-text typewritter">
                             Led by Lastrights Rumblecoin, a goblin so shrewd he'd charge you for the dirt your boots
                             carried into his office, the Undertakers had put out the call for additional hands. Or
                             claws. Or whatever appendages one might bring to the table. "Graveyard shifts ain't never
@@ -86,15 +87,15 @@
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-12">
                     <div class="card mt-8">
-                        <img src="/images/gob-kungfu.png" alt="Angry Goblins">
+                        <img src="/images/gob-kungfu.png" class="photos-article" alt="Angry Goblins">
                         <div id="gob-photo">Picture by F-stop Fizzlefreak</div>
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-6 col-sm-12">
                     <div class="card mt-8">
-                        <h5 class="card-title">Goblin Tools</h5>
+                        <h5 class="card-title">Mrgl Magah!</h5>
                         <p class="card-text">
-                            <img src="/images/mrgl-magah-00.png" class="img-fluid" alt="Mrgl Magah">
+                            <img src="/images/mrgl-magah-00.png" class="img-fluid photos-article" alt="Mrgl Magah">
                         </p>
                     </div>
                 </div>
@@ -104,7 +105,7 @@
             <div class="row news-columns-mid">
                 <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="card mt-8">
-                        <p class="card-text">
+                        <p class="card-text typewritter">
                             But why the sudden surge of wannabe undertakers? Well, it seems that the heroes of the
                             hardcore realms had been biting the dust with such frequency that the Undertakers' ledger
                             looked more like a tome than a tally sheet. "Heroes falling left and right, and every one of
@@ -112,7 +113,7 @@
                             trembling before their untimely demise, deserved a final farewell worthy of song—or at least
                             a footnote in "mrglglglgl."
                         </p>
-                        <p>
+                        <p class="card-text typewritter">
                             The Ratchet office, usually reserved for hush-hush deals and the occasional coin-counting
                             contest,
                             had been transformed into a raucous recruitment rally. Hopefuls arrived with shovels slung
@@ -124,16 +125,17 @@
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="card mt-8">
-                        <p class="card-text">
+                        <p class="card-text typewritter">
                             "It's all about efficiency," winked a wide-eyed applicant, unveiling a tombstone that
                             doubled as a dinner plate.
                         </p>
-                        <p> Lastrights surveyed the chaos with glee. Each new recruit meant more hands to dig, more
+                        <p class="card-text typewritter"> Lastrights surveyed the chaos with glee. Each new recruit
+                            meant more hands to dig, more
                             pockets to fill, and, most importantly, more eyes to witness his entrepreneurial empire. The
                             deaths, the destruction, the quivering humans meeting their end - it was all part of the
                             cycle of loot and life in Azeroth.
                         </p>
-                        <p>And at the center of it all, the Undermine Undertakers
+                        <p class="card-text typewritter">And at the center of it all, the Undermine Undertakers
                             reaped the rewards, their laughter echoing through the streets, mingling with the clinks of
                             their ill-gotten gains and the inevitable cries of those heroes yet to fall.
 
@@ -196,11 +198,15 @@
                             <span class="author">by Trixie Boomsparkle</span>
                         </div>
                         <h5 class="card-title">Scales and Tales: Ratchet's Murloc Bikini Contest Makes a Splash!</h5>
-                        RATCHET, Azshara Coast – It was a day that promised sun, surf, and a sensational array of scaled
-                        contestants as Ratchet played host to the first-ever Murloc Bikini Contest. The shoreline was
-                        awash with the latest in aquatic fashion, but as the waves rolled in, so did a tide of troubles
-                        that even the saltiest of seadogs hadn't seen coming.
-                        <p>
+                        <p class="card-text typewritter">
+                            RATCHET, Azshara Coast – It was a day that promised sun, surf, and a sensational array of
+                            scaled
+                            contestants as Ratchet played host to the first-ever Murloc Bikini Contest. The shoreline
+                            was
+                            awash with the latest in aquatic fashion, but as the waves rolled in, so did a tide of
+                            troubles
+                            that even the saltiest of seadogs hadn't seen coming.</p>
+                        <p class="card-text typewritter">
                             The event, dubbed "The Siren's Parade," was the brainchild of Glix Wavesmasher, a goblin
                             with an
                             eye for business and a love for the utterly unconventional. "Why should landwalkers have all
@@ -208,21 +214,21 @@
                             fun?" Glix proclaimed. But as the day unfolded, it became clear that what works in theory
                             can
                             often flounder on the shores of reality.</p>
-                        <p>
+                        <p class="card-text typewritter">
                             First to strut their stuff on the shell-studded catwalk were the Mur'glam twins, sporting
                             seaweed sashes and clamshell couture that drew gasps from the crowd. But the awe turned to
                             alarm
                             when their starfish accessories took on a life of their own, scuttling away and causing
                             quite
                             the commotion among the front-row spectators.</p>
-                        <p>
+                        <p class="card-text typewritter">
                             Midway through the contest, a particularly robust murloc contestant, Mrgl the Mighty,
                             presented
                             a stunning spectacle with an ensemble featuring live guppies. However, the fishy fashion
                             statement quickly backfished, turning the catwalk into a freestyle frenzy as the guppies
                             made a
                             break for open water, with Mrgl flailing fin over webbed foot to catch them.</p>
-                        <p>
+                        <p class="card-text typewritter">
                             The event spiraled into further pandemonium when the grand finale took an unexpected dive.
                             As
                             the reigning queen of the murlocs, Glrgl the Gleaming, made her appearance adorned in what
@@ -232,26 +238,27 @@
                             into darkness as overzealous fans inadvertently knocked over a generator in their
                             excitement.</p>
 
-                        <p> In the end, the murlocs didn't seem to mind the mishaps. They frolicked and caroled in their
+                        <p class="card-text typewritter"> In the end, the murlocs didn't seem to mind the mishaps. They
+                            frolicked and caroled in their
                             unique gurgling dialect, basking in the spotlight and the adoration of their fans. Glix
                             Wavesmasher, ever the opportunist, declared the event a "resounding success" amid the chaos,
                             vowing to make the Murloc Bikini Contest an annual tradition.</p>
-                        <p>
+                        <p class="card-text typewritter">
                             Despite the setbacks, one thing was clear: the combination of murloc fashionistas and goblin
                             entrepreneurial spirit had created an event that Ratchet would not soon forget. As the sun
                             set
                             on the sparkling sands, murmurs of next year's contest bubbled to the surface, with promises
                             of
                             bigger splashes and even more extravagant sea-inspired apparel.</p>
-                        <p>
+                        <p class="card-text typewritter">
                             For those who missed the spectacle, worry not. Plans are already in the works for a "Siren's
                             Encore," complete with life vests for the front rows and an emergency fish wrangler on
                             standby.
                             It's a scale tale for the ages, and this reporter will be here, quill in hand, to document
                             every
                             shimmering, shambling step.</p>
-
-                        Until then, keep your fins flirty and your scales sparkling, Ratchet!
+                        <p class="card-text typewritter">
+                            Until then, keep your fins flirty and your scales sparkling, Ratchet!</p>
                     </div>
                 </div>
             </div>
