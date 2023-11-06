9c7b36a57e3fbfb30377f3a1770ddcccafface6d
aaparky <aa.parky@gmail.com>
2023-11-06 11:03:53 +0000

    layout test

---
+++ index.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <div class="container">
+
+        <hr>
+
+        <div class="header">
+            <div id="flex-header-left">
+                <div>
+                    Morning Edition
+                </div>
+            </div>
+
+            <div id="flex-header-center">
+                <h2>The Undermine Undertaker's Update</h2>
+            </div>
+            <div id="flex-header-right">
+                Daily
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="sub-header">
+            <div id="sub-dt">
+                Date &amp; Time
+            </div>
+            <div id="sub-vol">
+                Volume # &amp; Issue #
+            </div>
+            <div id="sub-est">
+                EST.2023 1c
+            </div>
+        </div>
+
+        <hr>
+
+        <div>
+            <div>
+                <h1>HEADLINES</h1>
+            </div>
+        </div>
+
+        <div>
+
+            <div>
+                <div>
+                    <h4>Sub Headline</h4>
+                    <hr><img src="https://bootsnipp.com/bootstrap-builder/libs/builder/icons/image.svg" height="128"
+                        width="128">
+                    <p>By reporter name and title</p>
+                    <p>Here is the news article!Here is the news article!Here is the news article!Here is the news
+                        article!Here is the news article!Here is the news article!Here is the news article!Here is the
+                        news article!Here is the news article!Here is the news article!Here is the news article!Here is
+                        the news article!Here is the news article!Here is the news article!Here is the news article!</p>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <h4>text only</h4>
+                    <p>many many news!many many news!many many news!many many news!many many news!many many news!many
+                        many news!many many news!many many news!many many news!many many news!many many news!many many
+                        news!many many news!many many news!many many news!many many news!many many news!many many
+                        news!many many news!many many news!many many news!many many news!many many news!many many
+                        news!many many news!many many news!many many news!many many news!many many news!many many
+                        news!many many news!many many news!many many news!many many news!many many news!many many news!
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+        <footer>
+            <p>© the Undermine Undertaker's Update 2017</p>
+        </footer>
+
+    </div> <!-- /container -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
