eb64288ab326b144584e398e9bb94659f5fa1c0c
aaparky <aa.parky@gmail.com>
2023-11-10 15:08:00 +0000

    tables hide cols mobile

--- tables/soccer_division1_table.php
+++ tables/soccer_division1_table.php
@@ -1,93 +1,93 @@
 <div class="soccer-header">Division 1: The Champions Tier</div>
-                        <table class="table table-sm custom-bg typewritter">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Team</th>
-                                    <th scope="col">Played</th>
-                                    <th scope="col">Won</th>
-                                    <th scope="col">Lost</th>
-                                    <th scope="col">Drawn</th>
-                                    <th scope="col">For</th>
-                                    <th scope="col">Against</th>
-                                    <th scope="col">GD</th>
-                                    <th scope="col">PTS</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Brill Bruisers</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
+<table class="table table-sm custom-bg typewritter">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Team</th>
+            <th scope="col">Played</th>
+            <th scope="col">Won</th>
+            <th scope="col">Lost</th>
+            <th scope="col" class="d-none d-md-table-cell">Drawn</th>
+            <th scope="col" class="d-none d-md-table-cell">For</th>
+            <th scope="col" class="d-none d-md-table-cell">Against</th>
+            <th scope="col" class="d-none d-md-table-cell">GD</th>
+            <th scope="col">PTS</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Brill Bruisers</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td class="d-none d-md-table-cell">0</td>
+            <td class="d-none d-md-table-cell">0</td>
+            <td class="d-none d-md-table-cell">0</td>
+            <td class="d-none d-md-table-cell">0</td>
+            <td>0</td>
 
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Darkshore Dribblers</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Darkshore Dribblers</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td class="d-none d-md-table-cell">0</td>
+            <td class="d-none d-md-table-cell">0</td>
+            <td class="d-none d-md-table-cell">0</td>
+            <td class="d-none d-md-table-cell">0</td>
+            <td>0</td>
 
-                                </tr>
-                                <th scope="row">3</th>
-                                <td>Gadgetzan Goalrushers</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
+        </tr>
+        <th scope="row">3</th>
+        <td>Gadgetzan Goalrushers</td>
+        <td>0</td>
+        <td>0</td>
+        <td>0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td>0</td>
 
-                                </tr>
-                                <th scope="row">4</th>
-                                <td>Kharanos Kickers</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
+        </tr>
+        <th scope="row">4</th>
+        <td>Kharanos Kickers</td>
+        <td>0</td>
+        <td>0</td>
+        <td>0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td>0</td>
 
-                                </tr>
-                                <th scope="row">5</th>
-                                <td>Ratchet Reavers</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
+        </tr>
+        <th scope="row">5</th>
+        <td>Ratchet Reavers</td>
+        <td>0</td>
+        <td>0</td>
+        <td>0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td>0</td>
 
-                                </tr>
-                                <th scope="row">6</th>
-                                <td>Stormwind Stallions</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
+        </tr>
+        <th scope="row">6</th>
+        <td>Stormwind Stallions</td>
+        <td>0</td>
+        <td>0</td>
+        <td>0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td class="d-none d-md-table-cell">0</td>
+        <td>0</td>
 
-                                </tr>
-                            </tbody>
-                        </table>
+        </tr>
+    </tbody>
+</table>
